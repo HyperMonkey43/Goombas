1348d3750ec61ea21f315f9ee18b4b27527e562f
Abdullah Mardouf <Abdullahomardouf@gmail.com>
2023-11-06 14:20:01 +0100

    hh

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+
 
 app = Flask(__name__)
+cart_items = []  # Initialize an empty cart
 
 @app.route('/')
 def home():
@@ -10,9 +12,17 @@ def home():
 def menu():
     return render_template('menu.html')
 
-@app.route('/cart')
+
+@app.route('/cart', methods=['GET', 'POST'])
 def cart():
-    return render_template('cart.html')
+    global cart_items
+
+    if request.method == 'POST':
+        item_name = request.form.get('item_name')
+        cart_items.append(item_name)  # Add the item to the cart
+
+    return render_template('cart.html', cart_items=cart_items)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
