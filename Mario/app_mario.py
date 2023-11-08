c7548f0975c1322dbb1631d1696b9004d87b5131
catalin181 <catalin.irimia14@yahoo.com>
2023-11-08 13:29:48 +0100

    da

--- Mario/app_mario.py
+++ Mario/app_mario.py
@@ -2,18 +2,17 @@ from flask import Flask, render_template, request
 import json
 app = Flask(__name__)
 
-ceva = []
+smth = []
 
 @app.route('/add_data', methods = ['POST'])
 def add_data():
+    global smth
     data = request.get_json()
-    ceva  = json.loads(data)
-    print(data)
-    print(ceva[0]['name'])
+    smth.append(data)
     return {}
     
 @app.route('/')
 def index():
-   return render_template('index.html', items = ceva)
+   return render_template('index.html', items = smth)
 
 app.run(host = '145.93.49.97')
